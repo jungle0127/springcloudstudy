941dc3e03145744ee3af32952db8a1f42773113c
jungle0127 <arbor@foxmail.com>
2018-12-04 22:37:49 +0800

    added demo projects for cloud

--- registry/src/main/java/com/ps/registry/Application.java
+++ registry/src/main/java/com/ps/registry/Application.java
@@ -1,4 +1,15 @@
 package com.ps.registry;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableEurekaServer
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
 }
