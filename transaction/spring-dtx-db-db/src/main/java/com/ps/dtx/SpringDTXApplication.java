577b8ffd6b5bc8e57ff30cea66031c7ee6962345
jungle0127 <arbor@foxmail.com>
2018-12-16 20:40:19 +0800

    added new demo project for dst

---
+++ transaction/spring-dtx-db-db/src/main/java/com/ps/dtx/SpringDTXApplication.java
@@ -0,0 +1,11 @@
+package com.ps.dtx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDTXApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDTXApplication.class,args);
+    }
+}
