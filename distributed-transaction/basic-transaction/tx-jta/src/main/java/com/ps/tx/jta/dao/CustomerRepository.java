c2ac522758fa756f4e0718e2eed63d57ff0c3535
jungle0127 <arbor@foxmail.com>
2018-12-27 23:16:02 +0800

    added springboot 2 demo project

---
+++ distributed-transaction/basic-transaction/tx-jta/src/main/java/com/ps/tx/jta/dao/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.ps.tx.jta.dao;
+
+import com.ps.tx.jta.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    Customer findOneByUsername(String username);
+}
---
+++ distributed-transaction/basic-transaction/tx-jta/src/main/java/com/ps/tx/jta/service/CustomerServiceInCode.java
@@ -0,0 +1,47 @@
+package com.ps.tx.jta.service;
+
+import com.ps.tx.jta.dao.CustomerRepository;
+import com.ps.tx.jta.domain.Customer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+@Service
+public class CustomerServiceInCode {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private PlatformTransactionManager platformTransactionManager;
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    public Customer createCustomer(Customer customer) throws Exception {
+        if(customer.getId() != null){
+            throw new RuntimeException("User exists.");
+        }
+        DefaultTransactionDefinition defaultTransactionDefinition = new DefaultTransactionDefinition();
+        defaultTransactionDefinition.setIsolationLevel(TransactionDefinition.ISOLATION_DEFAULT);
+        defaultTransactionDefinition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        defaultTransactionDefinition.setTimeout(15);
+        TransactionStatus transactionStatus = this.platformTransactionManager.getTransaction(defaultTransactionDefinition);
+        try{
+            customer.setUsername("Code: " + customer.getUsername());
+            customerRepository.save(customer);
+            this.jmsTemplate.convertAndSend("customer:msg:reply",customer.getUsername());
+            platformTransactionManager.commit(transactionStatus);
+            return customer;
+        } catch (Exception e){
+            platformTransactionManager.rollback(transactionStatus);
+            logger.error("Got error:{}", e.getMessage());
+            throw e;
+        }
+
+    }
+}
