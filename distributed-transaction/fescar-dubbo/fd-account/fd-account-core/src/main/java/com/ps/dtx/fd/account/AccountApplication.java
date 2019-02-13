43639a88abf7809ba59fabcbecf4588505d0bfc8
jungle0127 <arbor@foxmail.com>
2019-02-13 12:16:38 +0800

    updated fdscar demo

--- distributed-transaction/fescar-dubbo/fd-account/fd-account-core/src/main/java/com/ps/dtx/fd/account/AccountApplication.java
+++ distributed-transaction/fescar-dubbo/fd-account/fd-account-core/src/main/java/com/ps/dtx/fd/account/AccountApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.ps.dtx.fd.account.dao.mapper")
 @EnableDubbo(scanBasePackages = "com.ps.dtx.fd.account.service")
 public class AccountApplication {
     public static void main(String[] args) {
---
+++ distributed-transaction/fescar-dubbo/fd-order/fd-order-core/src/main/java/com/ps/dtx/fd/order/config/DatasourceConfiguration.java
@@ -0,0 +1,49 @@
+package com.ps.dtx.fd.order.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.fescar.rm.datasource.DataSourceProxy;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatasourceConfiguration {
+    @Value("${spring.datasource.url}")
+    private String dataSourceUrl;
+    @Value("${spring.datasource.username}")
+    private String userName;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Bean
+    public DruidDataSource druidDataSource(){
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setUrl(this.dataSourceUrl);
+        druidDataSource.setUsername(this.userName);
+        druidDataSource.setPassword(this.password);
+        druidDataSource.setDriverClassName(this.driverClassName);
+        druidDataSource.setUseGlobalDataSourceStat(true);
+        return druidDataSource;
+    }
+    @Bean
+    public DataSource dataSourceProxy(){
+        return new DataSourceProxy(druidDataSource());
+    }
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSourceProxy());
+        return sqlSessionFactoryBean.getObject();
+    }
+    @Bean
+    public DataSourceTransactionManager dataSourceTransactionManager(){
+        return new DataSourceTransactionManager(dataSourceProxy());
+    }
+}
