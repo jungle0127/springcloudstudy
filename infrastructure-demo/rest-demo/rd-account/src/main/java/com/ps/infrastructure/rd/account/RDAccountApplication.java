9b6f4557820ca37c0e58c3d72fb7a48564b61f5e
jungle0127 <arbor@foxmail.com>
2019-02-16 23:07:44 +0800

    added new demo project

---
+++ infrastructure-demo/rest-demo/rd-account/src/main/java/com/ps/infrastructure/rd/account/RDAccountApplication.java
@@ -0,0 +1,11 @@
+package com.ps.infrastructure.rd.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RDAccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RDAccountApplication.class);
+    }
+}
