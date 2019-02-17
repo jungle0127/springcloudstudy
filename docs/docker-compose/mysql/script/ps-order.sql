dca74346ff585805826512e678b1005d739cb56c
zhang.jinghe <zhang.jinghe05016@westone.com.cn>
2019-02-17 16:54:21 +0800

    added docker-compose script for demo db

---
+++ docs/docker-compose/mysql/script/ps-order.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS `ps_order`;
+CREATE DATABASE `ps_order` DEFAULT CHARACTER SET utf8;
+
+USE ps_order;
+
+DROP TABLE IF EXISTS `t_order`;
+CREATE TABLE `t_order`(
+	`id` bigint auto_increment PRIMARY KEY,
+    `customer_id` bigint NOT NULL,
+    `storage_id` bigint NOT NULL,
+    `amount` int NOT NULL,
+    `order_desc` varchar(255),
+    `create_time` timestamp default now()
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
