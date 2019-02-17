278400dca8b864bd447c939650037d6b0e21825c
zhang.jinghe <zhang.jinghe05016@westone.com.cn>
2019-02-17 17:02:34 +0800

    updated demo sql

--- docs/docker-compose/mysql/script/ps-storage.sql
+++ docs/docker-compose/mysql/script/ps-storage.sql
@@ -2,6 +2,35 @@ DROP DATABASE IF EXISTS `ps_storage`;
 CREATE DATABASE `ps_storage` DEFAULT CHARACTER SET utf8;
 
 USE ps_storage;
+--
+-- alibaba official demo
+--
+DROP TABLE IF EXISTS `storage_tbl`;
+CREATE TABLE `storage_tbl` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `commodity_code` varchar(255) DEFAULT NULL,
+  `count` int(11) DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`commodity_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `undo_log`;
+CREATE TABLE `undo_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `branch_id` bigint(20) NOT NULL,
+  `xid` varchar(100) NOT NULL,
+  `rollback_info` longblob NOT NULL,
+  `log_status` int(11) NOT NULL,
+  `log_created` datetime NOT NULL,
+  `log_modified` datetime NOT NULL,
+  `ext` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_unionkey` (`xid`,`branch_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=159 DEFAULT CHARSET=utf8;
+
+--
+-- demo
+--
 DROP TABLE IF EXISTS `t_storage`;
 CREATE TABLE `t_storage`(
 	`id` bigint auto_increment primary key,
