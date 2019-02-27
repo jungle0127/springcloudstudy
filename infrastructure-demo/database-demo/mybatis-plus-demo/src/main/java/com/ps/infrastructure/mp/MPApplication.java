7b26121044b8452ec43e3fccfc2a998ecca33eea
zhang.jinghe <zhang.jinghe05016@westone.com.cn>
2019-02-27 17:31:21 +0800

    added new demo project and slides

---
+++ infrastructure-demo/database-demo/mybatis-plus-demo/src/main/java/com/ps/infrastructure/mp/MPApplication.java
@@ -0,0 +1,11 @@
+package com.ps.infrastructure.mp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MPApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MPApplication.class,args);
+    }
+}
